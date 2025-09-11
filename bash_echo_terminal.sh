593f0752c18c956a490ec72b34d3e463f86d8850
Molly Bright <32640425+BrightMG@users.noreply.github.com>
2025-09-11 12:03:26 -0500

    Molly trying to create conflicts
    Figuring this out slowly, added line, deleted character, etc.

--- bash_echo_terminal.sh
+++ bash_echo_terminal.sh
@@ -12,4 +12,5 @@ echo ""
 
 echo "Here is a list of awesome ANVIL members who are learning git:"
 echo "Molly, after many years a PI needs a refresher!"
-echo "Andrew should go here once we figure this out. (Andrew was here!)"
+echo "Andrew should go here once we figure this out. (Andrew was here)"
+echo "Molly's change is going here, trying to conflict with an updated main branch"
